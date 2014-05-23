16fa8113cb8b898c2fa22aa9d23eb38b3e2871bb
phette23 <phette23@gmail.com>
2014-05-23 14:59:58 -0400

    nevermind, sudo alias doesn't work anyways, remove it

--- inc/aliases.fish
+++ inc/aliases.fish
@@ -120,10 +120,6 @@ function spoton -d 'Enable Spotlight'
     sudo mdutil -a -i on
 end
 
-function sudo -d 'Enable aliases to be sudoed'
-    command sudo $argv
-end
-
 function urlencode -d 'URL-encode strings'
     python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);" $argv
 end
