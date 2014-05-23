2bdaf192f53fcba33758b3a7c5d6bd786ba0a1cc
phette23 <phette23@gmail.com>
2014-05-22 23:22:38 -0400

    add fs function

---
+++ functions/fs.fish
@@ -0,0 +1,13 @@
+function fs -d 'Determine size of a file or total size of a directory'
+    if du -b /dev/null > /dev/null ^&1
+        set arg -sbh
+    else
+        set arg -sh
+    end
+
+    if test $argv[1]
+        du $arg -- $argv
+    else
+        du $arg .[^.]* *
+    end
+end
