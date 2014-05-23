8d525243ada38053f4528d8cf71decda542a40a8
phette23 <phette23@gmail.com>
2014-05-23 14:34:46 -0400

    alphabetize aliases

--- inc/aliases.fish
+++ inc/aliases.fish
@@ -1,5 +1,53 @@
 # Simple functions that are basically aliases
 
+function .. -d 'Go up 1 dir'
+	cd ..
+end
+
+function ... -d 'Go up 2 dirs'
+	cd ../..
+end
+
+function .... -d 'Go up 3 dirs'
+	cd ../../..
+end
+
+function ..... -d 'Go up 4 dirs'
+	cd ../../../..
+end
+
+function '~' -d 'Go to home dir'
+	cd ~
+end
+
+function code -d 'Go to ~/code'
+	cd ~/code
+end
+
+function cx -d 'Make file executable'
+	chmod +x $argv
+end
+
+function d -d 'Go to Dropbox'
+    cd ~/Dropbox
+end
+
+function dl -d 'Go to Downloads'
+    cd ~/Downloads
+end
+
+function fns -d 'Shorthand for (functions)'
+  functions $argv
+end
+
+function g -d 'Alias for (git)'
+    git $argv
+end
+
+function gs -d 'Concise (git status)'
+    git status -sb
+end
+
 # Note: Fish uses caret (^) to redirect stderr
 if ls --color > /dev/null ^&1 # GNU `ls`
     set colorflag --color
@@ -23,61 +71,10 @@ function lsd -d 'List only directories'
   ls -lF $colorflag $argv | grep "^d" --color=never
 end
 
-# (functions) is too much typing
-function fns -d 'Shorthand for (functions)'
-  functions $argv
-end
-
-# Easier navigation
-function .. -d 'Go up 1 dir'
-	cd ..
-end
-
-function ... -d 'Go up 2 dirs'
-	cd ../..
-end
-
-function .... -d 'Go up 3 dirs'
-	cd ../../..
-end
-
-function ..... -d 'Go up 4 dirs'
-	cd ../../../..
-end
-
-function '~' -d 'Go to home dir'
-	cd ~
-end
-
-function d -d 'Go to Dropbox'
-	cd ~/Dropbox
-end
-
-function dl -d 'Go to Downloads'
-	cd ~/Downloads
-end
-
-function code -d 'Go to ~/code'
-	cd ~/code
-end
-
-# Misc common commands
-function cx -d 'Make file executable'
-	chmod +x $argv
-end
-
 function pgl -d 'Find matching running processes'
 	pgrep -lf $argv
 end
 
 function rf -d 'Remove recursively—careful!!!'
 	rm -rf $argv
-end
-
-function g -d 'Alias for (git)'
-	git $argv
-end
-
-function gs -d 'Concise (git status)'
-	git status -sb
 end
