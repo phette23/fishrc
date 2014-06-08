a54d090603d6a8aacba6bf998b95009ecc84abe2
phette23 <phette23@gmail.com>
2014-06-07 17:22:31 -0700

    path: fix adding paths each time new shell is opened

--- inc/path.fish
+++ inc/path.fish
@@ -1,1 +1,7 @@
-set -U fish_user_paths $fish_user_paths /usr/local/bin /usr/local/share/npm/bin /usr/local/pear/bin $HOME/bin
+# this basically only runs once, since Fish persists Universal vars across sessions
+for path in /usr/local/share/npm/bin /usr/local/pear/bin $HOME/bin /usr/local/bin
+    if not contains $path $fish_user_paths
+        # append path to var
+        set -U fish_user_paths $fish_user_paths $path
+    end
+end
