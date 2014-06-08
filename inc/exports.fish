25ccd4d0be6a0eaf9550db9f5a910bbfee4fbab9
phette23 <phette23@gmail.com>
2014-06-07 17:29:49 -0700

    exports: a few changes to man pages

--- inc/exports.fish
+++ inc/exports.fish
@@ -9,10 +9,7 @@ set -xU LANG en_US.UTF-8
 set -xU LC_ALL en_US.UTF-8
 
 # Highlight section titles in manual pages
-set -xU LESS_TERMCAP_md $ORANGE
-
-# Don’t clear the screen after quitting a manual page
-set -xU MANPAGER "less -x"
+set -xU LESS_TERMCAP_md (set_color yellow)
 
 # Always enable colored `grep` output
 set -xU GREP_OPTIONS '--color=auto'
