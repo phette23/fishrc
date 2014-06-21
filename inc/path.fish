de927f4f09cc43dcbeac9394a38dec5ffbff3288
phette23 <phette23@gmail.com>
2014-06-21 14:36:04 -0700

    path: append user paths to front; use /usr/local/bin over /usr/bin

--- inc/path.fish
+++ inc/path.fish
@@ -2,6 +2,6 @@
 for path in /usr/local/share/npm/bin /usr/local/pear/bin $HOME/bin /usr/local/bin
     if not contains $path $fish_user_paths
         # append path to var
-        set -U fish_user_paths $fish_user_paths $path
+        set -U fish_user_paths $path $fish_user_paths
     end
 end
