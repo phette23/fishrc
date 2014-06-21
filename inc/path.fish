cb7ed703e8251aa1aec8c61f899321b464f8d91b
phette23 <phette23@gmail.com>
2014-06-21 15:42:31 -0700

    path: last commit still didn't work; /usr/local/bin is def in front now

--- inc/path.fish
+++ inc/path.fish
@@ -1,7 +1,10 @@
 # this basically only runs once, since Fish persists Universal vars across sessions
-for path in /usr/local/share/npm/bin /usr/local/pear/bin $HOME/bin /usr/local/bin
+for path in /usr/local/share/npm/bin /usr/local/pear/bin $HOME/bin
     if not contains $path $fish_user_paths
         # append path to var
         set -U fish_user_paths $path $fish_user_paths
     end
 end
+
+# Ensure Homebrew bins are used over /usr/bin (e.g. git)
+set -x PATH /usr/local/bin $PATH
