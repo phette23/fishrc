e22b7de367f540f6b139e56d12742198c95874dc
phette23 <phette23@gmail.com>
2014-08-07 23:50:13 -0700

    fcheat: use new __cheat fns for Fish cheat sheet

--- functions/fcheat.fish
+++ functions/fcheat.fish
@@ -1,17 +1,8 @@
 function fcheat -d 'Cheat sheet for fish shell'
+    __cheat_heading 'Fish Cheat Sheet'
 
-    function msg
-        set_color D80; echo -n $argv[1]
-        set_color normal; echo -e $argv[2]
-    end
-
-    set_color purple
-    echo -e '\t\tFish Cheat Sheet\n'
-
-    msg 'fish_config' '\t-> launch web-based configuration'
-    msg 'help' '\t\t-> open documentation HTML'
-    msg 'math' '\t\t-> perform simple calculations'
-    msg 'vared $var' '\t-> interactively edit variable $var'
-    set_color D80; echo 'see also: fn fish_default_key_bindings'
-    set_color normal
+    __cheat_ln 'fish_config\t' 'launch web-based configuration'
+    __cheat_ln 'help\t\t' 'open documentation HTML'
+    __cheat_ln 'math\t\t' 'perform simple calculations'
+    __cheat_ln 'vared $var\t' 'interactively edit variable $var'
 end
