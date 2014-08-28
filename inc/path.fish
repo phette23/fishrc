377debe748b48f77d62cffcbb58bea826b261fef
phette23 <phette23@gmail.com>
2014-08-27 21:34:53 -0700

    path: move npm to ~/npm

--- inc/path.fish
+++ inc/path.fish
@@ -1,5 +1,5 @@
 # this basically only runs once, since Fish persists Universal vars across sessions
-for path in /usr/local/share/npm/bin /usr/local/pear/bin $HOME/bin
+for path in $HOME/npm/bin /usr/local/pear/bin $HOME/bin
     if not contains $path $fish_user_paths
         # append path to var
         set -U fish_user_paths $path $fish_user_paths
