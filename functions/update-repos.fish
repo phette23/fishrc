8f2b5b8894c377b097d23ffc4123f9ba3ef33718
phette23 <phette23@gmail.com>
2014-08-30 21:37:29 -0700

    update-repos function to update all subdir git repos

---
+++ functions/update-repos.fish
@@ -0,0 +1,25 @@
+function update-repos -d 'Update all git repos in sub-directories'
+    for dir in ./*/
+        cd $dir
+        # check if there's a repo
+        git status >/dev/null 2>1
+
+        if [ $status -eq 0 ]
+            # check that it has a remote origin
+            git remote | grep origin >/dev/null 2>1
+
+            if [ $status -eq 0 ]
+                set_color red
+                echo -n "Updating "
+                echo $dir | sed -e "s|\./||" -e "s|/||"
+                set_color normal
+                # be safe: fast-forward only pull
+                git pull --ff-only origin
+            end
+        end
+
+        # return to cwd
+        # note that ".." wouldn't work with symlinked dirs
+        cd -
+    end
+end
