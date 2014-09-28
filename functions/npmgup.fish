9559514810ebdc21379a12576567816c26a381c4
phette23 <phette23@gmail.com>
2014-09-28 15:37:49 -0700

    npmgup: fix sloppy Bash copy-paste, use Fish syntax

--- functions/npmgup.fish
+++ functions/npmgup.fish
@@ -1,6 +1,6 @@
 function npmgup -d 'Update all global NPM packages'
 
-    for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f2):
+    for package in (npm -g outdated --parseable --depth=0 | cut -d: -f2)
         npm -g install "$package"
     end
 
