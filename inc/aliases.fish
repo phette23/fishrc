f37660313269830716120a8a2efd3bf475105c6b
phette23 <phette23@gmail.com>
2015-01-10 14:43:18 -0800

    aliases - add nout alias for npm outdated

--- inc/aliases.fish
+++ inc/aliases.fish
@@ -74,6 +74,10 @@ function map -d 'Intuitive map function'
  xargs -n1 $argv
 end
 
+function nout -d 'npm outdated --depth=0'
+  npm outdated --depth=0 $argv
+end
+
 function nr -d 'npm run alias'
   npm run $argv
 end
