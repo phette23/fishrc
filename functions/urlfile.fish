ccd9386609fb2462b8cbaa7fa8341c554054cff1
phette23 <phette23@gmail.com>
2016-03-29 15:43:49 -0700

    fn to create .url files

---
+++ functions/urlfile.fish
@@ -0,0 +1,11 @@
+function urlfile -d 'given URL, echo a .url file (pipe fn ouput to file)'
+    # If no argument is passed, exit with error
+    if [ (count $argv) -eq 0 ]
+        echo 'no URL provided' >2
+        exit 1
+    else
+        echo "[InternetShortcut]"
+        # first arg is URL
+        echo "URL=\"$argv[1]\""
+    end
+end
