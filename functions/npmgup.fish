f8e61b470ae9d099e8ee8b9e834043a195f03dd6
phette23 <phette23@gmail.com>
2016-04-14 12:59:42 -0700

    npmgup - don't update linked packages

--- functions/npmgup.fish
+++ functions/npmgup.fish
@@ -1,6 +1,6 @@
 function npmgup -d 'Update all global NPM packages'
 
-    for package in (npm -g outdated --parseable --depth=0 | cut -d: -f2)
+    for package in (npm -g outdated --parseable --depth=0 | cut -d: -f2 | grep --invert-match '@linked$')
         npm -g install "$package"
     end
 
