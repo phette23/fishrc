dca7fe338037d4d1547ad7b6ac377d17405a7c74
phette23 <phette23@gmail.com>
2016-04-15 17:36:38 -0700

    npmgup - oops reinstalling current version, install without version string

--- functions/npmgup.fish
+++ functions/npmgup.fish
@@ -1,7 +1,7 @@
 function npmgup -d 'Update all global NPM packages'
 
-    for package in (npm -g outdated --parseable --depth=0 | cut -d: -f2 | grep --invert-match '@linked$')
-        npm -g install "$package"
+    for package in (npm -g outdated --parseable --depth=0 | cut -d: -f2 | grep --invert-match '@linked$' | cut -d@ -f1)
+        npm install -g "$package"
     end
 
 end
