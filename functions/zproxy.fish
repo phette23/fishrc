98520054b921426d982fc925466c9dc2e45fa794
phette23 <phette23@gmail.com>
2016-05-22 14:04:53 -0700

    rm zproxy

--- functions/zproxy.fish
+++
@@ -1,6 +0,0 @@
-# because Z isn't Fish-compatible we basically wrap a Bash script
-# save the destination Z returns, then cd to it
-function zproxy -d 'jump to dir using regex'
-    set dest (bash -c "source /usr/local/etc/profile.d/z.sh && _z $argv && pwd")
-    cd $dest
-end
