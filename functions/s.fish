32d040c5acd132eccacc00da3b93f4189d8767b5
phette23 <phette23@gmail.com>
2016-07-09 21:21:12 -0700

    rm subl shortcut, I don't use Sublime anymore

--- functions/s.fish
+++
@@ -1,8 +0,0 @@
-function s -d 'Alias for subl, sublime binary'
-    # If no argument is passed, open current dir
-    if [ (count $argv) -eq 0 ]
-        subl .
-    else
-        subl $argv
-    end
-end
