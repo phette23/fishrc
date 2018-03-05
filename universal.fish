352124603454c5375c5b9b8b24b52eeeeb810793
phette23 <phette23@gmail.com>
2018-03-05 12:54:09 -0800

    add homebrew python2 bin to path

--- universal.fish
+++ universal.fish
@@ -3,6 +3,7 @@
 set --universal fish_greeting ""
 
 set --local paths ~/bin /usr/local/bin /usr/local/sbin
+[ -d /usr/local/opt/python@2/bin ]; and set paths $paths /usr/local/opt/python@2/bin
 set --local gnubin (brew --prefix coreutils)/libexec/gnubin
 [ -d $gnubin ]; and set --local paths $paths $gnubin
 
