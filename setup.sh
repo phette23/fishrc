c09b845280534342d9c875d130b06821dc066baf
phette23 <phette23@gmail.com>
2019-09-03 16:05:14 -0700

    setup - homebrew/fisherman steps have changed

--- setup.sh
+++ setup.sh
@@ -4,11 +4,10 @@
 brew install fish
 echo '/usr/local/bin/fish' | sudo tee -a /etc/shells
 
-# install fisherman plugin manager
-brew tap fisherman/tap
-brew install fisherman
+# install fisherman plugin manager (this automatically adds "fisher" tap)
+brew install bbatsche/fisher/fisherman
 # install plugins
-fisher edc/bass z
+fisher add edc/bass jethrokuan/z
 
 # download iTerm2 integration file
 curl -L https://iterm2.com/misc/fish_startup.in > ~/.iterm2_shell_integration.fish
