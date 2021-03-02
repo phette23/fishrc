6c976c6d89d12f11ca35491310dd2e6709d5f306
Eric Phetteplace <phette23@gmail.com>
2021-03-02 09:05:25 -0800

    rm my one use of `^` to redirect stderr, that's deprecated

--- functions/diff.fish
+++ functions/diff.fish
@@ -1,5 +1,5 @@
 function diff -d 'Use Git’s colored diff when available'
-    hash git ^/dev/null
+    hash git 2>/dev/null
     if [ $status -eq 0 ]
         git diff --no-index --color-words $argv
     else
