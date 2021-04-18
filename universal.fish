ab26aa50458a833542abba6707116ee9514f4910
Eric Phetteplace <phette23@gmail.com>
2021-04-18 15:49:49 -0700

    I guess you still have to export universal vars

--- universal.fish
+++ universal.fish
@@ -11,5 +11,5 @@ if command -v brew >/dev/null
     brew --prefix coreutils >/dev/null; and fish_add_path (brew --prefix coreutils)/libexec/gnubin
 end
 
-set --universal EDITOR atom
-set --universal HOMEBREW_EDITOR atom
+set --universal --export EDITOR atom
+set --universal --export HOMEBREW_EDITOR atom
