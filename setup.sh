6bfe095b350b1990d9aa861cad55fc1f0968853e
phette23 <phette23@gmail.com>
2021-08-28 20:57:46 -0700

    slight code cleanup for setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # Install fish, add to /etc/shells, sync
 
-command -v fish 2>/dev/null || brew install fish
+command -v fish &>/dev/null || brew install fish
 grep fish /etc/shells >/dev/null || echo '/usr/local/bin/fish' | sudo tee -a /etc/shells
 
 # install fisherman plugin manager (this automatically adds "fisher" tap)
 brew install bbatsche/fisher/fisherman
 # install plugins
-fisher add edc/bass jethrokuan/z
+fisher add jethrokuan/z
 
 # download iTerm2 integration file
 curl -L https://iterm2.com/misc/fish_startup.in > ~/.iterm2_shell_integration.fish
