ed3823314969f39e7f96b323ee47b5c630a9d234
Eric Phetteplace <phette23@gmail.com>
2021-11-10 11:49:32 -0800

    config - no need to run `pipenv --completion` anymore
    homebrew's pipenv puts its completions in /usrlocal/share/fish/vendor_completions.d
    now so we don't have to do this extra step

--- config.fish
+++ config.fish
@@ -16,10 +16,7 @@ if command --query rbenv
     rbenv init - | source
 end
 
-# pipenv completion
-command --query pipenv && eval (pipenv --completion)
-
-# iTerm2 integration
+# iTerm 2 integration
 [ -e ~/.iterm2_shell_integration.fish ]; and source ~/.iterm2_shell_integration.fish
 
 # The next line updates PATH for the Google Cloud SDK.
