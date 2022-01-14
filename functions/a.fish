7a2f87b4488ce905f8e200e228e7d26d1da01d44
phette23 <phette23@gmail.com>
2022-01-14 12:12:29 -0800

    rm atom shortcut
    RIP an era :cry:

--- functions/a.fish
+++
@@ -1,8 +0,0 @@
-function a -d 'open file or cwd in Atom editor' --wraps=atom
-    # If no argument is passed, open current dir
-    if [ (count $argv) -eq 0 ]
-        atom .
-    else
-        atom $argv
-    end
-end
