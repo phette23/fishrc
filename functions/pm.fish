bd46fc42377dcc82f98364942a1b6336c10b4f1a
phette23 <phette23@gmail.com>
2022-03-30 09:34:27 -0700

    delete two unused aliases, pm and phpserver

--- functions/pm.fish
+++
@@ -1,8 +0,0 @@
-function pm -d 'alias for "python manage.py"'
-	# for libraries wagtail site where manage.py is in a subfolder
-	if test -e libraries/manage.py
-		python libraries/manage.py $argv
-	else
-		python manage.py $argv
-	end
-end
