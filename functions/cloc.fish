5ea41470dea29471dd2a496c11c29f166dda8d3c
phette23 <phette23@gmail.com>
2023-10-11 08:45:55 -0700

    cloc: skip lock files and cache dirs

--- functions/cloc.fish
+++ functions/cloc.fish
@@ -1,3 +1,3 @@
 function cloc -d 'Count Lines of Code (excluding certain dirs)' --wraps=cloc
-  command cloc --exclude-dir=node_modules,_site $argv
+    command cloc --exclude-dir=node_modules,_site,.pytest_cache,.sass-cache,__pycache__ --not-match-f='pnpm\-lock\.yaml' --not-match-f='package\-lock\.json' --not-match-f='Pipfile\.lock' --not-match-f='poetry\.lock' $argv
 end
