240e9b38862a0a955ce4ac33c482fb2720425a14
phette23 <phette23@gmail.com>
2023-11-08 12:45:48 -0800

    todos: detect both @TODO and TODO followed by space

--- functions/todos.fish
+++ functions/todos.fish
@@ -2,7 +2,7 @@ function todos -d 'Find @todo statements in a project'
     # test if ack is installed
     type -P ack >/dev/null
     if [ $status -eq 0 ]
-        command ack --ignore-case --context=2 '@todo' $argv
+        command ack --ignore-case --context=2 '@?todo\s' $argv
     else
         echo 'Ack not installed!'
         return 1
