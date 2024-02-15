f2a7eb2d864b9993b9658e9848d0aee97918c1c1
phette23 <phette23@gmail.com>
2024-02-15 10:42:16 -0800

    fix: mute stderr for code --update-extensions
    prints Electron deprecation warnings which are just noise

--- inc/mac.fish
+++ inc/mac.fish
@@ -15,7 +15,7 @@ end
 
 function upd -d 'Run common software update commands'
     cq brew && brew update && HOMEBREW_NO_ENV_HINTS=true brew upgrade
-    cq code && code --update-extensions
+    cq code && code --update-extensions 2>/dev/null
     cq gcloud && gcloud components update --quiet
     cq pipx && pipx upgrade-all
     cq pnpm && pnpm update --global --latest
