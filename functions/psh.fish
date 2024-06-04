4c86052b4e62ab9f420fbc26d897de02bce3ce27
phette23 <phette23@gmail.com>
2024-06-04 12:03:54 -0700

    fix: also don't activate mise in poetry shell

--- functions/psh.fish
+++ functions/psh.fish
@@ -1,6 +1,6 @@
 function psh -d "run pipenv/poetry shell (detects which one)"
     if [ -f Pipfile ]
-        pipenv shell
+        MISE_FISH_AUTO_ACTIVATE=0 pipenv shell
     else if [ -f pyproject.toml ]
         MISE_FISH_AUTO_ACTIVATE=0 poetry shell
     else
