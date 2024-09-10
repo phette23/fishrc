cb8503a724ace65e240691ba6bd247c4a16e8721
phette23 <phette23@gmail.com>
2024-09-10 10:39:47 -0700

    feat: psh works with uv now, too

--- functions/psh.fish
+++ functions/psh.fish
@@ -1,9 +1,16 @@
 function psh -d "run pipenv/poetry shell (detects which one)"
     if [ -f Pipfile ]
         MISE_FISH_AUTO_ACTIVATE=0 pipenv shell
-    else if [ -f pyproject.toml ]
+    # both poetry & uv use pyproject.toml so we look for their unique lock files
+    else if [ -f poetry.lock ]
         MISE_FISH_AUTO_ACTIVATE=0 poetry shell
+    else if [ -f uv.lock ]
+        if [ -d .venv ]
+            source .venv/bin/activate.fish
+        else
+            echo "uv project but no .venv found"
+        end
     else
-        echo "No Pipfile or pyproject.toml found"
+        echo "No Pipfile, poetry.lock, or uv.lock found"
     end
 end
