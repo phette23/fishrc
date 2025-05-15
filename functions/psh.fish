3c395622ab77d95f51128a3b8b32458d1b39ddf7
phette23 <phette23@gmail.com>
2025-05-15 09:58:46 -0700

    fix: poetry shell is deprecated, use env activate

--- functions/psh.fish
+++ functions/psh.fish
@@ -1,16 +1,13 @@
-function psh -d "run pipenv/poetry shell (detects which one)"
+function psh -d "activate python project virtualenv"
     if [ -f Pipfile ]
         MISE_FISH_AUTO_ACTIVATE=0 pipenv shell
     # both poetry & uv use pyproject.toml so we look for their unique lock files
     else if [ -f poetry.lock ]
-        MISE_FISH_AUTO_ACTIVATE=0 poetry shell
-    else if [ -f uv.lock ]
-        if [ -d .venv ]
+        poetry env activate | source
+    # uv and anything else that makes a .venv
+    else if [ -d .venv ]
             source .venv/bin/activate.fish
-        else
-            echo "uv project but no .venv found"
-        end
     else
-        echo "No Pipfile, poetry.lock, or uv.lock found"
+        echo "No Pipfile, poetry.lock, or .venv found"
     end
 end
