e3376379562c20a0aa5cbbc464f078c3d741ae50
phette23 <phette23@gmail.com>
2025-10-02 21:34:55 -0700

    fix: update tre cmd ignored dirs
    ignore standard python dirs like .venv, __pycache__
    remover bower_components from list, bower is defunct

--- functions/tre.fish
+++ functions/tre.fish
@@ -2,5 +2,5 @@ function tre -d 'Shorthand for tree with colors & hidden files enabled' --wraps 
     # Hidden files & color enabled, ignores `.git` & other common dirs, lists
     # directories first. Output is piped into `less` with options to preserve color
     # & line numbers, unless the output is small enough for one screen.
-    tree -aC -I '.git|node_modules|bower_components|.sass-cache' --dirsfirst $argv | less -FRNX
+    tree -aC -I '.git|node_modules|.venv|.sass-cache|__pycache__|.pytest_cache' --dirsfirst $argv | less -FRNX
 end
