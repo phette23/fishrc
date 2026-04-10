770794e5a8883c574974e8c1dfb4bff18477cc05
phette23 <phette23@gmail.com>
2026-04-10 09:55:54 -0700

    fix: new dir paths for music files

--- functions/music_sync.fish
+++ functions/music_sync.fish
@@ -7,13 +7,13 @@ function music_sync -d "Copy ~/Music files to external hard drive"
         return 1
     end
 
-    if [ ! -d ~/Music/Music ]
-        echo "Error: Source music directory '~/Music/Music' does not exist." >&2
+    if [ ! -d ~/Music/Media.localized/Music ]
+        echo "Error: Source music directory '~/Music/Media/Music' does not exist." >&2
         return 1
     end
 
     # Example command to sync music (replace with actual sync command)
-    rsync --archive --compress --exclude '.DS_Store' --human-readable --progress --update ~/Music/Music/ /Volumes/Arxiv/Music/Music
+    rsync --archive --compress --exclude '.DS_Store' --human-readable --progress --update ~/Music/Media.localized/Music/ /Volumes/Arxiv/Music
 
     if test $status -eq 0
         echo "Music synchronization completed."
